60f3c389d00a211339f6b580ca8c5dd0ebe39352
Angel <angelCL0@outlook.com>
2020-04-14 14:49:07 -0400

    container

--- pages/Playground.js
+++ pages/Playground.js
@@ -32,6 +32,7 @@ const Playground = () => (
               <button id="button2">Info &rarr;</button>
             </a>
           </div>
+
           <div className="Image">
             <img src="/park.svg" alt="Park Logo" width="100%" />
           </div>
@@ -42,146 +43,147 @@ const Playground = () => (
           </h3>
         </div>
       </main>
-    </div>
 
-    <style jsx>{`
-      .container {
-        min-height: 100vh;
-        padding: 0 0.5rem;
-        display: flex;
-        flex-direction: column;
-        justify-content: center;
-        align-items: center;
-      }
-
-      main {
-        padding: 5rem 0;
-        flex: 1;
-        display: flex;
-        flex-direction: column;
-        justify-content: center;
-        align-items: center;
-      }
-
-      .Hero {
-        max-width: 1120px;
-        display: grid;
-        grid-template-columns: repeat(2, 1fr);
-        padding: 80px 80px;
-      }
-
-      .subHero {
-        width: 100%;
-        background: linear-gradient(100.08deg, #2f80ed 0%, #9051e0 100%);
-         {
-          /* background: #ff9900; */
+      <style jsx>{`
+        .container {
+          min-height: 100vh;
+          padding: 0 0.5rem;
+          display: flex;
+          flex-direction: column;
+          justify-content: center;
+          align-items: center;
         }
-        padding: 20px 50px;
-      }
 
-      .subHero h3 {
-        font-size: 3rem;
-        line-height: 1.5;
-        color: white;
-        padding-left: 80px;
-        max-width: 800px;
-      }
+        main {
+          padding: 5rem 0;
+          flex: 1;
+          display: flex;
+          flex-direction: column;
+          justify-content: center;
+          align-items: center;
+        }
 
-      @media (max-width: 1020px) {
         .Hero {
-          grid-template-columns: repeat(1, 1fr);
-          grid-gap: 20px;
-          padding: 80px 20px;
+          max-width: 1120px;
+          display: grid;
+          grid-template-columns: repeat(2, 1fr);
+          padding: 80px 80px;
+        }
+
+        .subHero {
+          width: 100%;
+          background: linear-gradient(100.08deg, #2f80ed 0%, #9051e0 100%);
+           {
+            /* background: #ff9900; */
+          }
+          padding: 20px 50px;
+        }
+
+        .subHero h3 {
+          font-size: 3rem;
+          line-height: 1.5;
+          color: white;
+          padding-left: 80px;
+          max-width: 800px;
+        }
+
+        @media (max-width: 1020px) {
+          .Hero {
+            grid-template-columns: repeat(1, 1fr);
+            grid-gap: 20px;
+            padding: 80px 20px;
+          }
+
+          .HeroGroup {
+            text-align: center;
+          }
+        }
+
+        .HappyUser {
+          display: grid;
+          grid-template-columns: repeat(3, 3fr);
+        }
+
+        .HeroGroup h1 a {
+          color: blue;
+        }
+
+        .HeroGroup h1 span {
+          color: blue;
         }
 
         .HeroGroup {
-          text-align: center;
+          max-width: 500px;
         }
-      }
 
-      .HappyUser {
-        display: grid;
-        grid-template-columns: repeat(3, 3fr);
-      }
+        .Image {
+          max-width: 700px;
+        }
 
-      .HeroGroup h1 a {
-        color: blue;
-      }
+        .HeroGroup #button2 {
+          margin-top: 10px;
+          background: white;
+          color: black;
+          border: 1px solid #eaeaea;
+        }
 
-      .HeroGroup h1 span {
-        color: blue;
-      }
+        .HeroGroup #button2:hover {
+          background: black;
+          color: white;
+          box-shadow: 0 20px 40px rgba(0, 0, 0, 0.25);
+          transform: translateY(-3px);
+        }
 
-      .HeroGroup {
-        max-width: 500px;
-      }
+        .HeroGroup button {
+          background: blue;
+          border: none;
+          padding: 12px 32px;
+          text-decoration: none;
+          color: white;
+          font-size: 22px;
+          font-weight: 600;
+          border-radius: 28px;
+          cursor: pointer;
+          box-shadow: 0 10px 20px rgba(0, 0, 0, 0.15);
 
-      .Image {
-        max-width: 700px;
-      }
+          transition: 0.8s cubic-bezier(0.2, 0.8, 0.2, 1);
+        }
 
-      .HeroGroup #button2 {
-        margin-top: 10px;
-        background: white;
-        color: black;
-        border: 1px solid #eaeaea;
-      }
+        .HeroGroup button:hover {
+          background: linear-gradient(99.78deg, #4200ff 0.48%, #bd00ff 102.18%);
+          color: white;
+          box-shadow: 0 20px 40px rgba(0, 0, 0, 0.25);
+          transform: translateY(-3px);
+        }
 
-      .HeroGroup #button2:hover {
-        background: black;
-        color: white;
-        box-shadow: 0 20px 40px rgba(0, 0, 0, 0.25);
-        transform: translateY(-3px);
-      }
+        .HeroGroup h1 {
+          margin: 0;
+          line-height: 1.15;
+          font-size: 4rem;
+        }
 
-      .HeroGroup button {
-        background: blue;
-        border: none;
-        padding: 12px 32px;
-        text-decoration: none;
-        color: white;
-        font-size: 22px;
-        font-weight: 600;
-        border-radius: 28px;
-        cursor: pointer;
-        box-shadow: 0 10px 20px rgba(0, 0, 0, 0.15);
+        .HeroGroup a {
+          color: white;
+          text-decoration: none;
+        }
 
-        transition: 0.8s cubic-bezier(0.2, 0.8, 0.2, 1);
-      }
+        .HeroGroup p {
+          font-size: 1.5rem;
+          line-height: 1.5;
+        }
+      `}</style>
 
-      .HeroGroup button:hover {
-        background: linear-gradient(99.78deg, #4200ff 0.48%, #bd00ff 102.18%);
-        color: white;
-        box-shadow: 0 20px 40px rgba(0, 0, 0, 0.25);
-        transform: translateY(-3px);
-      }
-
-      .HeroGroup h1 {
-        margin: 0;
-        line-height: 1.15;
-        font-size: 4rem;
-      }
-
-      .HeroGroup a {
-        color: white;
-        text-decoration: none;
-      }
-
-      .HeroGroup p {
-        font-size: 1.5rem;
-        line-height: 1.5;
-      }
-    `}</style>
-
-    <style jsx global>{`
-      html,
-      body {
-        padding: 0;
-        margin: 0;
-        font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen,
-          Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif;
-      }
-    `}</style>
+      <style jsx global>{`
+        html,
+        body {
+          padding: 0;
+          margin: 0;
+          font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto,
+            Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue,
+            sans-serif;
+        }
+      `}</style>
+    </div>
   </Layout>
 );
 
