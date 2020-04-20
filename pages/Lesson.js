cf70db28de7cb9bfc364724ebaa340a29c0f2af1
Angel <angelCL0@outlook.com>
2020-04-19 23:59:52 -0400

    AL

--- pages/Lesson.js
+++ pages/Lesson.js
@@ -4,46 +4,43 @@ const Lesson = () => (
   <Layout>
     <div className="container">
       <main>
-        <div className="Parent">
-          <div className="Hero">
-            <h1>Learn how to create a heart shape</h1>
-            <p>
-              In this lesson I will show you how to create a heart shape in any
-              design tool. Simple and easy you will be able to implement it in a
-              few minutes.
-            </p>
-            <div className="HeroGroup">
-              <img src="/FigmaHeart.svg" />
-            </div>
-            <h3>Heart shape</h3>
-            <p>
-              Head over to your favorite design tool and start by making a
-              Square 140 x 140
-            </p>
-            <div className="HeroGroup">
-              <img src="/Square.svg" />
-            </div>
-            <p>
-              Next place a circle on each side of the square, make sure it's
-              divided in two, Again 140 x 140.
-            </p>
-            <div className="HeroGroup">
-              <img src="/Circles.svg" />
-            </div>
-            <p>
-              last but not least, Create a union selection and rotate it to
-              45deg.
-            </p>
-            <div className="HeroGroup">
-              <img src="/ShapeRotate.svg" />
-            </div>
-            <p>
-              There you have it a pretty heart shape! Hope to see you on the
-              next one!
-            </p>
-            <div className="HeroGroup">
-              <img src="/FigmaHeart.svg" />
-            </div>
+        <div className="Hero">
+          <h1>Learn how to create a heart shape</h1>
+          <p>
+            In this lesson I will show you how to create a heart shape in any
+            design tool. Simple and easy you will be able to implement it in a
+            few minutes.
+          </p>
+          <div className="HeroGroup">
+            <img src="/FigmaHeart.svg" />
+          </div>
+          <h3>Heart shape</h3>
+          <p>
+            Head over to your favorite design tool and start by making a Square
+            140 x 140
+          </p>
+          <div className="HeroGroup">
+            <img src="/Square.svg" />
+          </div>
+          <p>
+            Next place a circle on each side of the square, make sure it's
+            divided in two, Again 140 x 140.
+          </p>
+          <div className="HeroGroup">
+            <img src="/Circles.svg" />
+          </div>
+          <p>
+            last but not least, Create a union selection and rotate it to 45deg.
+          </p>
+          <div className="HeroGroup">
+            <img src="/ShapeRotate.svg" />
+          </div>
+          <p>
+            There you have it a pretty heart shape! Hope to see you on the next
+            one!
+          </p>
+          <div className="HeroGroup">
+            <img src="/FigmaHeart.svg" />
           </div>
         </div>
       </main>
@@ -68,13 +65,16 @@ const Lesson = () => (
         align-items: center;
       }
 
-      .Parent {
-        padding: 40px 20px;
-      }
-
       .Hero {
         max-width: 800px;
         margin: 0 auto;
+        padding: 80px;
+      }
+
+      @media (max-width: 620px) {
+        .Hero {
+          padding: 0 20px;
+        }
       }
 
       .Hero h3 {
